5f42c1f6b4795b1f4ed3a6978660cb181ecb0350
Francesc Alted <francesc@blosc.org>
2026-02-10 12:38:57 +0100

    Fixing windows issues (III)

--- src/minctest.h
+++ src/minctest.h
@@ -3,7 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "miniexpr.h"
+
+#if defined(_WIN32) || defined(_WIN64)
+static int minctest_setenv(const char *name, const char *value, int overwrite) {
+    if (!name || !value || name[0] == '\0' || strchr(name, '=') != NULL) {
+        return -1;
+    }
+    if (!overwrite) {
+        char *existing = NULL;
+        size_t existing_len = 0;
+        if (_dupenv_s(&existing, &existing_len, name) == 0 && existing != NULL) {
+            free(existing);
+            return 0;
+        }
+        free(existing);
+    }
+    return _putenv_s(name, value);
+}
+
+static int minctest_unsetenv(const char *name) {
+    if (!name || name[0] == '\0' || strchr(name, '=') != NULL) {
+        return -1;
+    }
+    return _putenv_s(name, "");
+}
+
+#ifndef setenv
+#define setenv(name, value, overwrite) minctest_setenv((name), (value), (overwrite))
+#endif
+#ifndef unsetenv
+#define unsetenv(name) minctest_unsetenv((name))
+#endif
+#endif
 
 #ifndef ME_EVAL_CHECK
 #define ME_EVAL_CHECK(expr, vars, n, out, count) \
