e114153e79baa4469efd36995b23532a3b59b5d1
Francesc Alted <francesc@blosc.org>
2026-02-14 08:46:35 +0100

    Fixing windows linking issues for side projects (II)

--- scripts/copy_first_existing.cmake
+++ scripts/copy_first_existing.cmake
@@ -2,6 +2,11 @@ if(NOT DEFINED src1 OR NOT DEFINED src2 OR NOT DEFINED dst)
   message(FATAL_ERROR "copy_first_existing.cmake requires src1, src2 and dst")
 endif()
 
+# Normalize accidental surrounding quotes from -Dsrc="..." style arguments.
+foreach(_v src1 src2 dst)
+  string(REGEX REPLACE "^\"(.*)\"$" "\\1" ${_v} "${${_v}}")
+endforeach()
+
 set(_picked "")
 if(EXISTS "${src1}")
   set(_picked "${src1}")
