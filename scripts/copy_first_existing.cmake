0eb167409cb1e542c4817c837cfe52e90eb697f8
Francesc Alted <francesc@blosc.org>
2026-02-14 08:43:19 +0100

    Fixing windows linking issues for side projects

---
+++ scripts/copy_first_existing.cmake
@@ -0,0 +1,20 @@
+if(NOT DEFINED src1 OR NOT DEFINED src2 OR NOT DEFINED dst)
+  message(FATAL_ERROR "copy_first_existing.cmake requires src1, src2 and dst")
+endif()
+
+set(_picked "")
+if(EXISTS "${src1}")
+  set(_picked "${src1}")
+elseif(EXISTS "${src2}")
+  set(_picked "${src2}")
+else()
+  message(FATAL_ERROR "Neither source exists: '${src1}' nor '${src2}'")
+endif()
+
+execute_process(
+  COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${_picked}" "${dst}"
+  RESULT_VARIABLE _copy_rc
+)
+if(NOT _copy_rc EQUAL 0)
+  message(FATAL_ERROR "Failed copying '${_picked}' to '${dst}'")
+endif()
